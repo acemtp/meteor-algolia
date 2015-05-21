f63cf6e6efb8b556797cdfac0c1e607249e4169d
Vianney Lecroart <acemtp@gmail.com>
2015-05-21 14:42:28 +0200

    add submodule use new npm algolia package (previous one is deprecated) update to latest version 3.3.2 of algolia

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'acemtp:algolia',
   summary: 'Package to handle the blazing fast Algolia search egnine (client and server)',
-  version: '1.7.5',
+  version: '3.3.2',
   git: 'https://github.com/acemtp/meteor-algolia.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles('server.js', 'server');
-  api.addFiles('algoliasearch.js', 'client');
+  api.addFiles('algoliasearch-client-js/dist/algoliasearch.js', 'client');
   api.export('Algolia', 'server');
   api.export('AlgoliaSearch', 'client');
 });
 
 Npm.depends({
-  'algolia-search': '1.7.5'
+  'algoliasearch': '3.3.2'
 });
